ea7b93c4d2e7c2ab20f1cad5e93421a0584384c0
Jenny Klich <jennyklich@gmail.com>
2020-09-06 13:50:37 -0600

    Add progress

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -6,9 +6,32 @@ require './lib/potluck'
 class PotluckTest < Minitest::Test
 
   def test_it_exists
-    potluck = Potluck.new("7-13-18", [])
+    potluck = Potluck.new("7-13-18")
     assert_equal "7-13-18", potluck.date
     assert_equal [], potluck.dishes
   end
 
+  def test_it_has_a_dish
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    assert_equal "Couscous Salad", couscous_salad.name
+    assert_equal "Cocktail Meatballs", cocktail_meatballs.name
+  end
+
+  def test_can_add_dish_to_dishes
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    assert_instance_of Dish, potluck.dishes[0]
+    assert_instance_of Dish, potluck.dishes[1]
+  end
+
+  def test_how_many_dishes
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal 2, potluck.dishes.length
+  end
 end
